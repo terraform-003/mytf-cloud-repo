fec009070cfb74dea7d448cf7e5f2969b038fba3
terraform-003 <terraform-003@outlook.com>
2025-02-20 13:02:34 +0000

    Region Name Amended to East US

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 resource "azurerm_resource_group" "cloudrg" {
-    location = "australiaeast"
+    location = "eastus"
     name = "mytf-cloud-rg"
     
 }
 
-resource "azurerm_storage_account" "cloudstoract" {
-    account_replication_type = "LRS"
-    account_tier = "Standard"
-    location = azurerm_resource_group.cloudrg.name
-    name = "mytfcloudstorage"
-    resource_group_name = azurerm_resource_group.cloudrg.location
+# resource "azurerm_storage_account" "cloudstoract" {
+#     account_replication_type = "LRS"
+#     account_tier = "Standard"
+#     location = azurerm_resource_group.cloudrg.name
+#     name = "mytfcloudstorage"
+#     resource_group_name = azurerm_resource_group.cloudrg.location
     
-}
+# }
 
 resource "azurerm_resource_group" "cloudrg2" {
-    location = "uk west"
+    location = "ukwest"
     name = "ukw-mytf-cloud-rg"
     
 }
