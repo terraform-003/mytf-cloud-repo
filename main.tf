a9b1ef78049ac8acd23ce528b7759d7e165f9ea7
terraform-003 <terraform-003@outlook.com>
2025-02-20 13:09:09 +0000

    Storage Account Name and Location Changed

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ resource "azurerm_resource_group" "cloudrg" {
     
 }
 
-# resource "azurerm_storage_account" "cloudstoract" {
-#     account_replication_type = "LRS"
-#     account_tier = "Standard"
-#     location = azurerm_resource_group.cloudrg.name
-#     name = "mytfcloudstorage"
-#     resource_group_name = azurerm_resource_group.cloudrg.location
+resource "azurerm_storage_account" "cloudstoract" {
+    account_replication_type = "LRS"
+    account_tier = "Standard"
+    location = azurerm_resource_group.cloudrg.name
+    name = "stor20025jks1"
+    resource_group_name = azurerm_resource_group.cloudrg.location
     
-# }
+}
 
 resource "azurerm_resource_group" "cloudrg2" {
     location = "ukwest"
