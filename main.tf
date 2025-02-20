9181923ddc5773ffe3d5ebdd1260c8e59738795c
terraform-003 <terraform-003@outlook.com>
2025-02-20 13:15:48 +0000

    Remove All Resources

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-resource "azurerm_resource_group" "cloudrg" {
-    location = "eastus"
-    name = "mytf-cloud-rg"
+# resource "azurerm_resource_group" "cloudrg" {
+#     location = "eastus"
+#     name = "mytf-cloud-rg"
     
-}
+# }
 
-resource "azurerm_storage_account" "cloudstoract" {
-    account_replication_type = "LRS"
-    account_tier = "Standard"
-    location = azurerm_resource_group.cloudrg.location
-    name = "stor20025jks1"
-    resource_group_name = azurerm_resource_group.cloudrg.name
+# resource "azurerm_storage_account" "cloudstoract" {
+#     account_replication_type = "LRS"
+#     account_tier = "Standard"
+#     location = azurerm_resource_group.cloudrg.location
+#     name = "stor20025jks1"
+#     resource_group_name = azurerm_resource_group.cloudrg.name
     
-}
+# }
 
-resource "azurerm_resource_group" "cloudrg2" {
-    location = "ukwest"
-    name = "ukw-mytf-cloud-rg"
+# resource "azurerm_resource_group" "cloudrg2" {
+#     location = "ukwest"
+#     name = "ukw-mytf-cloud-rg"
     
-}
+# }
